2acafcead083c2f1147043cb459d56a0e6e7fde2
Vinay <vinaykumarg@virtusa.com>
2018-11-27 17:38:56 +0530

    backup 2

---
+++ src/pages/home/home.ts
@@ -0,0 +1,68 @@
+import { Component } from '@angular/core';
+import { NavController, ActionSheetController } from 'ionic-angular';
+import { Camera, PictureSourceType } from '@ionic-native/camera';
+import * as Tesseract from 'tesseract.js'
+import { NgProgress } from '@ngx-progressbar/core';
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+ 
+  selectedImage: string;
+  imageText: string;
+ 
+  constructor(public navCtrl: NavController, private camera: Camera, private actionSheetCtrl: ActionSheetController, public progress: NgProgress) {
+  }
+ 
+  selectSource() {    
+    let actionSheet = this.actionSheetCtrl.create({
+      buttons: [
+        {
+          text: 'Use Library',
+          handler: () => {
+            this.getPicture(this.camera.PictureSourceType.PHOTOLIBRARY);
+          }
+        }, {
+          text: 'Capture Image',
+          handler: () => {
+            this.getPicture(this.camera.PictureSourceType.CAMERA);
+          }
+        }, {
+          text: 'Cancel',
+          role: 'cancel'
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+ 
+  getPicture(sourceType: PictureSourceType) {
+    this.camera.getPicture({
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      sourceType: sourceType,
+      allowEdit: true,
+      saveToPhotoAlbum: false,
+      correctOrientation: true
+    }).then((imageData) => {
+      this.selectedImage = `data:image/jpeg;base64,${imageData}`;
+    });
+  }
+ 
+  recognizeImage() {
+    Tesseract.recognize(this.selectedImage)
+    .progress(message => {
+      // if (message.status === 'recognizing text')
+      // this.progress.set(message.progress);
+    })
+    .catch(err => console.error(err))
+    .then(result => {
+      this.imageText = result.text;
+    })
+    .finally(resultOrError => {
+      // this.progress.complete();
+    });
+  }
+ 
+}
